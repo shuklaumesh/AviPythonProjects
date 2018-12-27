3b29c69a2b1dedfa2bf6be26bb277153ea611cd8
umesh <umeshdataman@hotmail.com>
2018-12-26 21:43:38 -0600

    Commited on 26. Dec 2018 AD

--- guessing_game_fun_v3.py
+++ guessing_game_fun_v3.py
@@ -1,1 +1,30 @@
-print('testing testing 123')
+"""We are going to make a guessing game in this file and in it's later vesions!"""
+"""This is a modified version of the earlier versions called guessing_game_fun_v3.py!"""
+"""This is the ultimate guessing_game_fun_v3.pymaster."""
+
+import random
+
+computers_number = random.randint(1,100)
+PROMPT = 'What is your guess? '
+
+def do_guess_round():
+    """Choose a random number, ask the user for a guess
+    check whether the guess is true
+    and reapeat until user is correct"""
+    computers_number = random.randint(1,100)#Added
+    while True:
+        players_guess = input(PROMPT)
+        if computers_number == int(players_guess):
+            print('Correct!')
+            break
+        elif computers_number > int(players_guess):
+            print('Too low')
+        else:
+            print('Too high')
+while True:
+    #Print statments added
+    print("Starting a new round")
+    print("The computer's number should be "+str(computers_number))
+    print("Let the guessing begin!!!")
+    do_guess_round()
+    print("")#blank line
